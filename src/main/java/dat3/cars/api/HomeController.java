f81c47a95844c57a8d19f54b102e8b4634d9a8a5
mura0717 <mura0717@stud.kea.dk>
2023-03-10 18:08:09 +0100

    Fixed missing annotation

--- src/main/java/dat3/cars/api/HomeController.java
+++ src/main/java/dat3/cars/api/HomeController.java
@@ -1,10 +1,12 @@
 package dat3.cars.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
+    @GetMapping("/")
     public RedirectView home() {
         return new RedirectView("/swagger-ui/index.html");
     }
